53fec464828ac0c926805593e65d1226efc7f0df
Nikos Mouchtaris <mouchtaris@gmail.com>
2014-10-19 19:13:43 +0300

    Patch-handle quiting

--- src/github.com/mouchtaris/topcoder_gocache/parser/action/quit.go
+++ src/github.com/mouchtaris/topcoder_gocache/parser/action/quit.go
@@ -3,7 +3,10 @@ package action
 import (
     "github.com/mouchtaris/topcoder_gocache/lex"
     "github.com/mouchtaris/topcoder_gocache/command"
+    "errors"
 )
+
+var ErrQuit = errors.New("quiting")
 
 //
 type Quit struct {
@@ -23,13 +26,10 @@ func (*Quit) Name () string {
 
 //
 func (action *Quit) Parse (lex *lex.Lexer) error {
-    comm := command.Quit { }
-
     err := lex.ReadEOC()
     if err != nil {
         return err
     }
 
-    action.consumer <- &comm
-    return nil
+    return ErrQuit
 }
